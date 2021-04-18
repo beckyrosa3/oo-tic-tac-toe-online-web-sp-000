c5da423870cd8579172c93f12e5b017840a60983
Becky Rosa <becky_m_rosa@yahoo.com>
2021-04-18 03:33:48 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -11,16 +11,18 @@ class TicTacToe
       [6,4,2]
     ]
 
+    attr_reader :board
+
     def initialize
       @board = Array.new(9," ")
     end
 
     def display_board
-      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts " #{board[0]} | #{board[1]} | #{board[2]} "
       puts "-----------"
-      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts " #{board[3]} | #{board[4]} | #{board[5]} "
       puts "-----------"
-      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+      puts " #{board[6]} | #{board[7]} | #{board[8]} "
     end
 
     def input_to_index(user_input)
